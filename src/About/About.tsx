cd096cffb08e013731673918079aeb58f8d43499
James Kadunc <kaduncjames@gmail.com>
2020-07-09 08:31:19 -0700

    Update about tsx

--- src/About/About.tsx
+++ src/About/About.tsx
@@ -2,11 +2,9 @@ import React from 'react';
 import {  Container, Grid, Hidden, Typography} from '@material-ui/core';
 import siteContent from '../siteContent'
 
-const aboutText = [
-  `Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.`,
-  `Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.`
-];
 
+
+const content = siteContent.about.content;
 
 function About() {
   return (
@@ -21,7 +19,7 @@ function About() {
       </Hidden>
       <Grid item xs={12} md={6}>
         <Typography variant='body1'>
-          {aboutText[0]}
+          {siteContent.about[0].}
         </Typography>
         </Grid>
       </Grid>
